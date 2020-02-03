f7ecbb5de05fb445e5dedb06356ff59e8e69b296
abcde090 <229243386@qq.com>
2020-02-03 23:42:04 +1000

    Debug.

--- src/utils/db.js
+++ src/utils/db.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 exports.connectToDB = () => {
     const {DB_HOST,DB_PORT,DB_DATABASE} =process.env;
     const connectionString =`mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
-
+    console.log(`Connecting to ${connectionString}`);
     mongoose.set('useFindAndModify',false);
     return mongoose.connect(process.env.MONGODB_URI || connectionString,{
         useNewUrlParser:true,
